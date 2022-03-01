42c3e0d18cb9749ef97ef8034c19805ffdca1a6d
nicolas <nicolasczaikowski@gmail.com>
2022-02-28 21:04:33 -0300

    Aplicando Html para criação de uma Calculadora

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
+var path = require('path');
 var calculation = require('./calculation');
 var calc = new calculation();
 
-
 var express = require('express');
 var app = new express();
+var viewsPath = path.join(__dirname, 'views');
+
+app.set('view engine', 'ejs');
+app.set('views', viewsPath);
 
 var arr = [
     3,100,20,10,4
 ];
+
+
+app.get('/',function(req, res){
+    res.render('calculadora');
+});
 
 app.get('/soma-menores-valores', function(req, res) {    
     // let ret = sumTwoSmallestNumbers(arr);
@@ -16,10 +25,11 @@ app.get('/soma-menores-valores', function(req, res) {
 });
 
 app.get('/soma-dois-valores', function(req, res) {    
-   var valor1 = parseInt(req.query.valor1); 
-   var valor2 = parseInt(req.query.valor2);
+   var valor1 = parseInt(req.query.numero1); 
+   var valor2 = parseInt(req.query.numero2);
    var total = calc.sumTwoValues(valor1, valor2);
    res.send('Soma: ' + total);
 });
+
 
 app.listen(9000, () => console.log('express started at http://localhost:9000'));
