df47e46cf8a0504d68bd3a41149f2993a75864e8
nicolas <nicolasczaikowski@gmail.com>
2022-02-28 21:53:42 -0300

    Aplicando Html para criação de uma Calculadora usandometodo POST

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 var path = require('path');
+var bodyParser = require('body-parser');
 var calculation = require('./calculation');
 var calc = new calculation();
 
 var express = require('express');
 var app = new express();
+
 var viewsPath = path.join(__dirname, 'views');
 
 app.set('view engine', 'ejs');
 app.set('views', viewsPath);
+app.use(bodyParser());
 
 var arr = [
     3,100,20,10,4
@@ -24,9 +27,10 @@ app.get('/soma-menores-valores', function(req, res) {
     res.send('Soma: ' + ret);
 });
 
-app.get('/soma-dois-valores', function(req, res) {    
-   var valor1 = parseInt(req.query.numero1); 
-   var valor2 = parseInt(req.query.numero2);
+app.post('/soma-dois-valores', function(req, res) {    
+   var valor1 = parseInt(req.body.numero1); 
+   var valor2 = parseInt(req.body.numero2);
+    console.log(req.body);
    var total = calc.sumTwoValues(valor1, valor2);
    res.send('Soma: ' + total);
 });
